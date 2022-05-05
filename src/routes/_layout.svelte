42a35ee0a21038b033e1e3ab42e2a4d5c1808a2b
Qcode122 <lloydsparrow01@gmail.com>
2022-05-05 20:07:56 +0100

    cleanup app

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -6,17 +6,4 @@
 
 <Nav {segment} />
 
-<main>
-  <slot />
-</main>
-
-<style>
-  main {
-    position: relative;
-    max-width: 56em;
-    background-color: white;
-    padding: 2em;
-    margin: 0 auto;
-    box-sizing: border-box;
-  }
-</style>
+<slot />
